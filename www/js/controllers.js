8b67886ef5116e0ad6fbb1bbce996ebf614d8bec
Mashnoor <nmmashnoor@gmail.com>
2015-12-09 01:04:05 +0600

    Getting Text From Server Experiment :D

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,4 +4,19 @@ angular.module('starter.controllers', [])
     {
         alert("hello world");
     }
+})
+.controller('MainCtrl', function($scope, $http){
+    //Get Server Text
+    var on_response = function(response)
+    {
+      var header_texts = response.data;
+        for(var key in header_texts )
+            {
+                $scope.header_text = header_texts[key]["ad"];
+                console.log(key);
+                setTimeout(2000);
+            }
+    };
+    var server_text_url = "http://104.131.22.246/dev/smartdsefiles/header_text.txt";
+    $http.get(server_text_url).then(on_response);
 });
