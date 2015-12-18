ba64563c0444dc0d1737f7ce4381d018ceb538a7
Mashnoor <nmmashnoor@gmail.com>
2015-12-18 23:39:16 +0600

    Use Experimental Directive to use header and footer.. :3

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,13 +10,20 @@ angular.module('starter.controllers', [])
     var on_response = function(response)
     {
       var header_texts = response.data;
+      var header_text_array = [];
         for(var key in header_texts )
             {
-                $scope.header_text = header_texts[key]["ad"];
-                console.log(key);
-                setTimeout(2000);
+                header_text_array.push(header_texts[key]["ad"]);
+               
+            }
+        
+      var no_of_header_text = header_text_array.length;
+        for(var i = 0; i < no_of_header_text; i++)
+            {
+                console.log(header_text_array[i]);
             }
     };
     var server_text_url = "http://104.131.22.246/dev/smartdsefiles/header_text.txt";
     $http.get(server_text_url).then(on_response);
 });
+;
